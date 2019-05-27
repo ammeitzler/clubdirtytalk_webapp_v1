30800ed4d175c4af920733ba50d388c5bd3a5492
Angeline <ammeitzler@gmail.com>
2019-05-27 17:25:58 -0400

    added host

--- api/settings/production.py
+++ api/settings/production.py
@@ -3,7 +3,7 @@ import os
 
 print("production ---------")
 
-ALLOWED_HOSTS = ['0.0.0.0', '18.224.94.174']
+ALLOWED_HOSTS = ['0.0.0.0', '18.224.94.174', '3.18.105.6']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', True)
